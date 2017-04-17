900875a496165cdb384cc256f446fc159ff5394c
grynca <grynca@gmail.com>
2017-04-17 15:38:23 +0200

    updated emscripten build

--- emscripten_build.sh
+++ emscripten_build.sh
@@ -7,8 +7,8 @@ EMCC_DEBUG=2
 IDIRS="-Iinclude -I/home/grynca/DEV/gamedev/base/include -I/home/grynca/DEV/gamedev/assets/include -I/home/grynca/DEV/libs/emscripten/glm"
 LDIRS=""
 LIBS="-lGLEW -lGL -lGLU"
-FLAGS="-std=c++11 -DWEB -DGLM_FORCE_RADIANS $IDIRS -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s USE_LIBPNG=1 -s TOTAL_MEMORY=134217728 --use-preload-plugins --preload-file data -DVISUAL_UPDATE"
-FLAGS+=" -DNDEBUG -O3"
+FLAGS="-std=c++14 -DWEB -DGLM_FORCE_RADIANS $IDIRS -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s USE_LIBPNG=1 -s TOTAL_MEMORY=134217728 --use-preload-plugins --preload-file data"
+FLAGS+=" -DUSE_SDL2 -DNDEBUG -O3"
 TEST_SOURCES="test/main.cpp"
 
 emcc $TEST_SOURCES $FLAGS -o build/Emscripten/test.bc
